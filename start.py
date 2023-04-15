d3d77bc145069a42e69ba7a4d98f8799ed49635f
C1328047 Allan Bontempo Taveira Siqueira <c1328047@interno.bb.com.br>
2023-04-15 00:28:27 -0300

    Salesforce Script Data Extract

---
+++ start.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+import pandas as pd
+import requests
+from simple_salesforce import Salesforce
+from io import StringIO
+
+# LOGIN CREDENTIALS
+username = ''  # Put your e-mail
+password = ''  # Put your password
+security_token = ''  # Put your token. You can get on configurations in salesforce
+
+sf = Salesforce(username=username, password=password, security_token=security_token)
+sessionId = sf.session_id
+start_time = datetime.now()
+
+# report URL structure
+
+orgParams = 'https://<YOUR COMPANY>.my.salesforce.com/'  # you can see this in your Salesforce URL
+exportParams = '?isdtp=p1&export=1&enc=UTF-8&xf=csv'
+
+# DOWNLOAD
+reportId = ''  # Choose the report and get the id
+
+reportUrl = orgParams + reportId + exportParams
+reportReq = requests.get(reportUrl, headers=sf.headers, cookies={'sid': sf.session_id})
+reportData = reportReq.content.decode('utf-8-sig')
+reportReadCsv = pd.read_csv(StringIO(reportData))
+
+reportReadCsv.to_csv('./NAME.csv')
+
+end_time = datetime.now()
+print(f'Donwload Complete. Duration: {end_time - start_time}')
